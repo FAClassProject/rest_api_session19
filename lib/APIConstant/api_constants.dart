8730fffe4021418aafb4d35c9181c94d69b53ace
FAClassProject <faclassaptech@gmail.com>
2023-04-12 12:23:27 +0100

    comment remed

--- lib/APIConstant/api_constants.dart
+++ lib/APIConstant/api_constants.dart
@@ -1,4 +1,9 @@
 class ApiConstants {
   static String baseUrl = 'https://jsonplaceholder.typicode.com';
+
+
+
   static String usersEndpoint = '/users';
+  static String todosEndpoint = '/todos';
+  static String photosEndpoint = '/photos';
 }
--- lib/api_service/api_service.dart
+++ lib/api_service/api_service.dart
@@ -5,13 +5,14 @@ import '../model/user_model.dart';
 import 'package:http/http.dart'as http;
 
 class ApiService {
-
   Future<List<UserModel>?> getUsers() async {
     try {
       var url = Uri.parse(ApiConstants.baseUrl + ApiConstants.usersEndpoint);
       var response = await http.get(url);
       if (response.statusCode == 200) {
+        print("printing response Body+++++++++++++${response.body}");
         List<UserModel> _model = userModelFromJson(response.body);
+        print("Model Length===============${_model.length}");
         return _model;
       }
     } catch (e) {
